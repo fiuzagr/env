50fee18f2b2259a00ebbc516aaefbff80642e009
Guilherme Fiuza <gui.fiuza@gmail.com>
2016-02-01 16:25:15 -0200

    PHPBREW

--- oh-my-zsh/custom/exports.zsh
+++ oh-my-zsh/custom/exports.zsh
@@ -1,6 +1,14 @@
 export ANT_HOME="$HOME/Programs/apache-ant-1.9.4"
 export ANDROID_HOME="$HOME/Library/Android/sdk"
-export PATH="$ANT_HOME/bin:$ANDROID_HOME:$ANDROID_HOME/tools:$PATH"
+export LOCAL_BIN="$HOME/.local/bin"
+export PATH="$ANT_HOME/bin:$ANDROID_HOME:$ANDROID_HOME/tools:$LOCAL_BIN:$PATH"
 
+
+# NVM
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+
+# PHPBREW
+export PHPBREW_HOME="$HOME/.phpbrew"
+[[ -e "$PHPBREW_HOME/bashrc" ]] && source "$PHPBREW_HOME/bashrc"
